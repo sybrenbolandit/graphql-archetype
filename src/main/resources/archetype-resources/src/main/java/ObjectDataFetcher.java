def559ea5f31ca15bb6641713f7e60c40d6e2cb1
Sybren Boland <sybren.boland@ns.nl>
2020-11-29 17:26:52 +0100

    Add working graphql api setup

---
+++ src/main/resources/archetype-resources/src/main/java/ObjectDataFetcher.java
@@ -0,0 +1,15 @@
+package $package;
+
+import ${package}.domain.DTO;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class ObjectDataFetcher implements DataFetcher<DTO> {
+    @Override
+    public DTO get(DataFetchingEnvironment dataFetchingEnvironment) {
+        return new DTO("Name");
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/domain/DTO.java
@@ -0,0 +1,19 @@
+package ${package}.domain;
+
+public class DTO {
+
+    private String name;
+
+    public DTO(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
